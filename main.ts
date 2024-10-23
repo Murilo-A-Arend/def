e96f91623874603c851ede9e761187f6f476e0d6
Murilo-A-Arend <126829427+Murilo-A-Arend@users.noreply.github.com>
2024-10-23 11:32:46 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
-    if (true) {
-        info.setScore(info.countdown())
-        game.gameOver(false)
-        info.changeScoreBy(game.runtime())
-    } else {
-        info.setScore(info.countdown())
-        game.gameOver(true)
-        game.setGameOverEffect(true, effects.confetti)
-    }
+info.onCountdownEnd(function () {
+    info.setScore(game.runtime() * 0.001)
+    game.setGameOverMessage(true, "Parabens " + classe)
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
 })
-let classe = game.askForString("Qual o seu nome?")
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(game.runtime() * 0.001)
+    game.setGameOverMessage(false, "Sobreviveu " + game.runtime() * 0.001 + " segundos")
+    game.gameOver(false)
+})
+let classe = ""
+classe = game.askForString("Qual o seu nome?")
 game.splash("Olá, " + classe)
 scene.setBackgroundColor(8)
 let inimigo = sprites.create(img`
@@ -30,6 +31,114 @@ let inimigo = sprites.create(img`
     ........ccccfcdbbcc........fff..
     .............fffff..............
     `, SpriteKind.Enemy)
+animation.runImageAnimation(
+inimigo,
+[img`
+    .................ccfff..............
+    ................cddbbf..............
+    ...............cddbbf...............
+    ..............fccbbcf............ccc
+    ........ffffffccccccff.........ccbbc
+    ......ffbbbbbbbbbbbbbcfff.....cdbbc.
+    ....ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+    ....fbcbbbbbffbbcbcbbbcccccfffdbbf..
+    ....fbbb1111ff1bcbcbbbcccccccbbbcf..
+    .....fb11111111bbbbbbcccccccccbccf..
+    ......fccc33cc11bbbbccccccccfffbbcf.
+    .......fc131c111bbbcccccbdbc...fbbf.
+    ........f33c111cbbbfdddddcc.....fbbf
+    .........ff1111fbdbbfddcc........fff
+    ...........cccccfbdbbfc.............
+    .................fffff..............
+    `,img`
+    .................ccfff..............
+    ................cddbbf..............
+    ...............cddbbf...............
+    .........ffffffccbbcf...............
+    ......fffbbbbbbbbcccff..............
+    .....fbbbbbbbbbbbbbbbcfff......ccccc
+    .....bcbbbbbffbcbcbbbbcccff...cdbbbc
+    .....bbb1111ffbbcbcbbbcccccffcddbbc.
+    .....fb11111111bcbcbbbcccccccbdbbf..
+    ......fccc33c11bbbbbbcccccccccbbcf..
+    .......fc131cc11bbbbccccccccffbccf..
+    ........f33c1111bbbcccccbdbc..fbbcf.
+    .........ff1111cbbbfdddddcc....fbbf.
+    ...........ccc1fbdbbfddcc.......fbbf
+    ..............ccfbdbbfc..........fff
+    .................fffff..............
+    `,img`
+    ..................ccfff.............
+    .................cddbbf.............
+    ........fffffffffddbbf..............
+    .......fbbbbbbbbbfcbcf..............
+    .......fbbc111bffbbccffff...........
+    .......fb111111ffbbbbbcccff....ccccc
+    ........f1cc3311bbcbcbbccccf..cdbbbc
+    ........fcc131c1bbbcbcbcccccfcddbbc.
+    .........f111111bbbcbcbccccccbdbbf..
+    .........f1111111bbbbbccccccccbbcf..
+    ..........f111111bbbbcccccccffbccf..
+    ...........c1111cbbbcccccbdbc.fbbcf.
+    ............cc11cbbbfddddddc...fbbf.
+    ..............cffbdbbfdddcc.....fbbf
+    .................fbdbbfcc........fff
+    ..................fffff.............
+    `,img`
+    ....................ccfff...........
+    ..........fffffffffcbbbbf...........
+    .........fbbbbbbbbbfffbf............
+    .........fbb111bffbbbbff............
+    .........fb11111ffbbbbbcff..........
+    .........f1cccc11bbcbcbcccf.........
+    ..........fc1c1c1bbbcbcbcccf...ccccc
+    ............c3331bbbcbcbccccfccddbbc
+    ...........c333c1bbbbbbbcccccbddbcc.
+    ...........c331c11bbbbbcccccccbbcc..
+    ..........cc13c111bbbbccccccffbccf..
+    ..........c111111cbbbcccccbbc.fccf..
+    ...........cc1111cbbbfdddddc..fbbcf.
+    .............cccffbdbbfdddc....fbbf.
+    ..................fbdbbfcc......fbbf
+    ...................fffff.........fff
+    `,img`
+    ...........fffffff...ccfff..........
+    ..........fbbbbbbbffcbbbbf..........
+    ..........fbb111bbbbbffbf...........
+    ..........fb11111ffbbbbff...........
+    ..........f1cccc1ffbbbbbcff.........
+    ..........ffc1c1c1bbcbcbcccf........
+    ...........fcc3331bbbcbcbcccf..ccccc
+    ............c333c1bbbcbcbccccfcddbbc
+    ............c333c1bbbbbbbcccccddbcc.
+    ............c333c11bbbbbccccccbbcc..
+    ...........cc331c11bbbbccccccfbccf..
+    ...........cc13c11cbbbcccccbbcfccf..
+    ...........c111111cbbbfdddddc.fbbcf.
+    ............cc1111fbdbbfdddc...fbbf.
+    ..............cccfffbdbbfcc.....fbbf
+    ....................fffff........fff
+    `,img`
+    ....................................
+    ....................................
+    ....................................
+    ...............ccffff...............
+    ..............cddbbbf...............
+    .......ffffffcddbbbf................
+    .....ffbbbbbbbbbbbbbcfff.......ccccc
+    ...ffbbbbbbbbcbcbbbbbcccff....cdbbbc
+    ..fbbbbbbbbbbcbbcbbbbcccccfffcddbbc.
+    .fbcbbbbbbbbbbcbcbbbbccccccccbdbbf..
+    .fbbbbbbbfffbbcbbbbbccccccccccbbcf..
+    .ffbb1111fffbbcbbbbcccccccbcffbccf..
+    ..ff111111111bbbbccccccbbbcc..fbbcf.
+    ....ccccccc111bdbbbfddbccc.....ffbbf
+    ........ccccccfbdbbbfcc..........fff
+    ...............ffffff...............
+    `],
+100,
+true
+)
 inimigo.setPosition(randint(scene.screenWidth(), scene.screenHeight()), randint(scene.screenWidth(), scene.screenHeight()))
 let jogador = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -49,7 +158,81 @@ let jogador = sprites.create(img`
     . . . . . . f f f f d d d c . . 
     . . . . . . . . . . c c c . . . 
     `, SpriteKind.Player)
+animation.runImageAnimation(
+jogador,
+[img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . . . 
+    . . . c 4 d 4 4 4 4 4 1 c . c c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+    . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+    . f 4 4 4 4 1 c 4 f 4 d f f f f 
+    . . f f 4 d 4 4 f f 4 c f c . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c c c c . . . . 
+    . . . . . . c d d d d d c . . . 
+    . . . . . . c c c c c d c . . . 
+    . . . . . c 4 4 4 4 d c c . . . 
+    . . . . c d 4 4 4 4 4 1 c . . . 
+    . . . c 4 4 1 4 4 4 4 4 1 c . . 
+    . . c 4 4 4 4 1 4 4 4 4 1 c c c 
+    . c 4 4 4 4 4 1 c c 4 4 1 4 4 c 
+    . c 4 4 4 4 4 1 4 4 f 4 1 f 4 f 
+    f 4 4 4 4 f 4 1 c 4 f 4 d f 4 f 
+    f 4 4 4 4 4 4 1 4 f f 4 f f 4 f 
+    . f 4 4 4 4 1 4 4 4 4 c b c f f 
+    . . f f f d 4 4 4 4 c d d c . . 
+    . . . . . f f f f f c c c . . . 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . c c 
+    . . . c 4 d 4 4 4 4 4 1 c c 4 c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 1 c f 4 4 1 f 4 f 
+    f 4 4 4 f 4 1 c 4 f 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f f f 
+    . f 4 4 4 4 1 c c 4 4 d f f . . 
+    . . f f 4 d 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c . . . 
+    . . . . . . . c c d d d d c . . 
+    . . . . . c c c c c c d d c . . 
+    . . . c c c 4 4 4 4 d c c c c c 
+    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+    . . f 4 4 1 4 c c 4 4 d f . . . 
+    . . . f d 4 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `],
+100,
+true
+)
 controller.moveSprite(jogador)
 jogador.setStayInScreen(true)
 inimigo.follow(jogador, 50)
-info.startCountdown(10)
+info.startCountdown(5)
